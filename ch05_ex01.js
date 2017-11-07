eb757f5d54adbf3f04ffdcef7e908b8a281c4488
Neethu Mohandas <prayaganeethu@gmail.com>
2017-11-07 13:54:33 +0530

    Arrayflattening using reduce - modified

--- ch05_ex01.js
+++ ch05_ex01.js
@@ -1,3 +1,3 @@
-let arrays = [[1, 2, 3], [4, 5], [6]];
-
-console.log(arrays.reduce(function(flatten, current) { return flatten.concat(current); }));
+var arrays = [[1, 2, 3], [4, 5], [6]]
+console.log(arrays.reduce((flat, array) => { return flat.concat(array) }, []))
+// → [1, 2, 3, 4, 5, 6]
