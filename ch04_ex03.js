7aebd30ae810121eb0a624cc8b779a748573f823
Neethu Mohandas <prayaganeethu@gmail.com>
2017-11-07 10:49:00 +0530

    Rewrote exercise 3 of chapter 4

--- ch04_ex03.js
+++ ch04_ex03.js
@@ -1,35 +1,44 @@
-function arrayToList(arr) {
-	let list = null;
-	for(let i = arr.length - 1;i >= 0;i--) {
-		list = {value: arr[i], rest: list};
-	}
-	return list;
+function arrayToList (arr, obj) {
+  if (obj === undefined) obj = {}
+  if (arr.length === 1) {
+    obj.value = arr[0]
+    obj.rest = null
+    return obj
+  }
+  obj.value = arr[0]
+  obj.rest = arrayToList(arr.slice(1), {})
+  return obj
 }
 
-function listToArray(list) {
-	let arr = [];
-	for(var i = list; i ; i = i.rest) {
-		arr.push(i.value);
-	}
-	return arr;
+function listToArray (obj, arr) {
+  if (arr === undefined) arr = []
+  if (obj.rest === null) {
+    arr.push(obj.value)
+    return arr
+  }
+  arr.push(obj.value)
+  arr = listToArray(obj.rest, arr)
+  return arr
 }
 
-function prepend(element, list) {
-	list = {value: element, rest: list};
-	return list;
+function prepend (element, obj) {
+  let list = {}
+  list.value = element
+  list.rest = obj
+  return list
 }
 
-function nth(list, number) {
-if(!list) {
-	return undefined;
-} else if (number === 0) {
-	return list.value;
-} else {
-	return nth(list.rest, number - 1);
-}
+function nth (list, index) {
+  if (!list) return undefined
+  if (index === 0) return list.value
+  return nth(list.rest, index - 1)
 }
 
-console.log(arrayToList([1,2,3]));
-console.log(listToArray(arrayToList([1, 2, 3])));
-console.log(prepend(1, prepend(2, null)));
-console.log(nth(arrayToList([1, 2, 3]), 1));
+console.log(arrayToList([10, 20]))
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(listToArray(arrayToList([10, 20, 30])))
+// → [10, 20, 30]
+console.log(prepend(10, prepend(20, null)))
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(nth(arrayToList([10, 20, 30]), 1))
+// → 20
