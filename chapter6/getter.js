78d2a681472f0a18e7f432deeb53b2bf59893473
Neethu Mohandas <prayaganeethu@gmail.com>
2017-11-14 11:46:11 +0530

    used defineProperty method to define a getter

--- chapter6/getter.js
+++ chapter6/getter.js
@@ -1,11 +1,12 @@
 let geekSkool = {
-  names: ['Neethu', 'Donna', 'Isha', 'Soumya'],
-  get newMember () {
-    if (this.names.length === 0) return undefined
-    return this.names[this.names.length - 1]
-  }
+  names: ['Neethu', 'Donna', 'Isha', 'Soumya']
 }
 
-delete geekSkool.newMember
+Object.defineProperty(geekSkool, 'newMember', { get: function () {
+  if (this.names.length === 0) return undefined
+  return this.names[this.names.length - 1]
+}})
+
+// delete geekSkool.newMember
 
 console.log(geekSkool.newMember)
