133781b89a099f830bfddf2e0b3dbf45f5d68d73
Neethu Mohandas <prayaganeethu@gmail.com>
2017-11-14 14:14:15 +0530

    example for instanceof operator

---
+++ chapter6/instanceOf.js
@@ -0,0 +1,10 @@
+function Geek () {}
+function Noob () {}
+
+let neethu = new Geek()
+let dummass = new Noob()
+
+console.log(neethu instanceof Geek)
+console.log(dummass instanceof Noob)
+console.log(neethu instanceof Object)
+console.log(dummass instanceof Geek)
