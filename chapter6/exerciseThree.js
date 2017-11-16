9c1f1ca869238a8a6f687939f0edbc9293d1e6c8
Neethu Mohandas <prayaganeethu@gmail.com>
2017-11-16 13:59:47 +0530

    Finished exercise 3

---
+++ chapter6/exerciseThree.js
@@ -0,0 +1,45 @@
+function ArraySeq (array) {
+  this.array = array
+}
+
+ArraySeq.prototype.length = function () {
+  let length = 0
+  let array = this.array
+  while (array[0] !== undefined) {
+    length++
+    array = array.slice(1)
+  }
+  return length
+}
+
+ArraySeq.prototype.current = function (pos) {
+  return this.array[pos]
+}
+
+function logFive (seq) {
+  for (let i = 0; i < 5 && i < seq.length(); i++) console.log(seq.current(i))
+}
+
+function RangeSeq (from, to) {
+  let array = []
+  for (let i = from; i <= to; i++) array.push(i)
+  this.array = array
+}
+
+RangeSeq.prototype.length = function () {
+  let length = 0
+  let array = this.array
+  while (array[0] !== null && array[0] !== undefined) {
+    length++
+    array = array.slice(1)
+  }
+  return length
+}
+
+RangeSeq.prototype.current = function (pos) {
+  return this.array[pos]
+}
+
+logFive(new ArraySeq([1, 2]))
+
+logFive(new RangeSeq(100, 1000))
